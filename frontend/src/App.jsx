35ef94641a57b841d5328dba67606c94bdb84935
Rahul Kumar Thakur <rahulthakur.official6@gmail.com>
2025-03-17 13:45:22 +0530

    two_d

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,3 @@
-// import "./App.scss";
-// import Contact from "./components/contact/Contact";
-// import Footer from "./components/Footer/Footer";
-// import Hero from "./components/Hero/Hero";
-// import Navbar from "./components/navbar/Navbar";
-// import Project from "./components/Project/Project";
-// import Services from "./components/Services/Services";
-// import TwoD_Staging from "./components/TwoD_Staging/TwoD_Staging";
-
-// const App = () => {
-//   return (
-//     <div>
-//       <section id="Homepage">
-//         <Navbar />
-//         <Hero />
-//       </section>
-//       <section id="Services">
-//         <Services />
-//       </section>
-//       <div id="Projects">
-//         <Project />
-//       </div>
-//       <div id="TwoD_Staging">
-//         <TwoD_Staging />
-//       </div>
-//       <section id="Contact">
-//         <Contact />
-//       </section>
-//       <section id="Footer">
-//         < Footer/>
-//       </section>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
-
 import { useEffect } from "react";
 import "./App.scss";
 import Contact from "./components/contact/Contact";
@@ -46,6 +7,7 @@ import Navbar from "./components/navbar/Navbar";
 import Project from "./components/Project/Project";
 import Services from "./components/Services/Services";
 import TwoD_Staging from "./components/TwoD_Staging/TwoD_Staging";
+import Experience from "./components/Experience/Experience";
 
 const App = () => {
   // useEffect(() => {
@@ -101,6 +63,9 @@ const App = () => {
       <div id="TwoD_Staging">
         <TwoD_Staging />
       </div>
+      {/* <section id="Experience">
+        <Experience />
+      </section> */}
       <section id="Contact">
         <Contact />
       </section>
